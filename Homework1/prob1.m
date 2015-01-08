0c15f7c17c0c93110302baaa9419735804a235aa
zackattacknyu <zackattacknyu@gmail.com>
2015-01-07 17:23:33 -0800

    more progress on prob 1

--- Homework1/prob1.m
+++ Homework1/prob1.m
@@ -6,17 +6,48 @@ X = iris(:,1:end-1);
 numFeatures = size(X,2);
 numDataPoints = size(X,1);
 
+%put features into vectors
+feature1 = X(:,1);
+feature2 = X(:,2);
+feature3 = X(:,3);
+feature4 = X(:,4);
+
 %part B
 figure
 subplot(2,2,1)
-hist(X(:,1))
+hist(feature1)
 title('Histogram for Feature 1')
 subplot(2,2,2)
-hist(X(:,2))
+hist(feature2)
 title('Histogram for Feature 2')
 subplot(2,2,3)
-hist(X(:,3))
+hist(feature3)
 title('Histogram for Feature 3')
 subplot(2,2,4)
-hist(X(:,4))
+hist(feature4)
 title('Histogram for Feature 4')
+
+%part C
+mean1 = mean(feature1);
+mean2 = mean(feature2);
+mean3 = mean(feature3);
+mean4 = mean(feature4);
+
+
+%part D
+
+%compute the variance
+var1 = var(feature1);
+var2 = var(feature2);
+var3 = var(feature3);
+var4 = var(feature4);
+
+%compute the standard deviation
+std1 = std(feature1);
+std2 = std(feature2);
+std3 = std(feature3);
+std4 = std(feature4);
+
+%part E
+% Normalizes the data
+%normalize1 = 
