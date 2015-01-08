fe49c9e0f27f60928cf8083f48b4342eb4652ccd
zackattacknyu <zackattacknyu@gmail.com>
2015-01-07 17:15:24 -0800

    renaming

--- Homework1/HW1_fromIhler/@gaussBayesClassify/setProbs.m
+++
@@ -1,5 +0,0 @@
-function obj = setProbs(obj,p)
-% gbc = setProbs(gbc, p) : set the covariances of the classifier
-
-obj.probs = p; 
-
--- Homework1/HW1_fromIhler/@knnClassify/train.m
+++
@@ -1,6 +0,0 @@
-function obj=train(obj, Xtr,Ytr)
-% knn=train(knn,Xtrain,Ytrain) : Batch training for knn; just memorize data
-  obj.Xtrain = Xtr;
-  obj.Ytrain = Ytr;
-  obj.classes= unique(Ytr);
-
