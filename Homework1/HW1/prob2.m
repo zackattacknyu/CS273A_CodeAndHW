13913465372dba2ac46fb268caa96faf3bc97078
zackattacknyu <zackattacknyu@gmail.com>
2015-01-08 14:49:57 -0800

    modified code slightly

--- Homework1/HW1/prob2.m
+++ Homework1/HW1/prob2.m
@@ -5,9 +5,13 @@ X=iris(:,1:end-1);
 [X y] = shuffleData(X,y); % shuffle data randomly
 [Xtr Xte Ytr Yte] = splitData(X,y, .75); % split data into 75/25 train/test
 
+%gets the first 2 features
+XtrFirstTwo = Xtr(:,1:2);
+XteFirstTwo = Xte(:,1:2);
+
 %train the classifier
 K = 3;
-knn = knnClassify( Xtr(:,1:2), Ytr, K ); % replace or set K to some integer
-YteHat = predict( knn, Xte(:,1:2) ); % make predictions on Xtest
+knn = knnClassify( XtrFirstTwo, Ytr, K ); % replace or set K to some integer
+YteHat = predict( knn, XteFirstTwo ); % make predictions on Xtest
 
-plotClassify2D( knn, Xtr(:,1:2), Ytr ); % make 2D classification plot with data (Xtr,Ytr)
+plotClassify2D( knn, XtrFirstTwo, Ytr ); % make 2D classification plot with data (Xtr,Ytr)
