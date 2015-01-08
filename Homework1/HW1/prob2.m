14b16deb635d99cc92713b1d5a3add594b927fb1
zackattacknyu <zackattacknyu@gmail.com>
2015-01-08 15:11:55 -0800

    started part B

--- Homework1/HW1/prob2.m
+++ Homework1/HW1/prob2.m
@@ -1,3 +1,4 @@
+%InitialPart
 iris=load('data/iris.txt'); 
 y=iris(:,end); 
 X=iris(:,1:end-1);
@@ -9,14 +10,14 @@ X=iris(:,1:end-1);
 XtrFirstTwo = Xtr(:,1:2);
 XteFirstTwo = Xte(:,1:2);
 
-
-K = 3;
 %knn = knnClassify( XtrFirstTwo, Ytr, K ); % replace or set K to some integer
 %YteHat = predict( knn, XteFirstTwo ); % make predictions on Xtest
 
 %plotClassify2D( knn, XtrFirstTwo, Ytr ); % make 2D classification plot with data (Xtr,Ytr)
 
-%part A
+%partA
+%{
+
 figure
 Kvals = [1,5,10,50];
 for i=1:4
@@ -30,3 +31,25 @@ for i=1:4
    plotClassify2D( knn, XtrFirstTwo, Ytr );
    title(strcat('K=',num2str(K),' classification plot'));
 end
+
+%}
+%part B
+
+K = 50;
+learner = knnClassify( XtrFirstTwo, Ytr, K );
+Yhat = predict(learner,XtrFirstTwo);
+errTrain=length(find(Yhat~=Ytr))/length(Ytr);
+%errTrain(i) = ... % TODO: " " to count what fraction of predictions are wrong
+
+%{
+Kvals=[1,2,5,10,50,100,200];
+for i=1:length(Kvals)
+    K = Kvals(i);
+    learner = knnClassify( XtrFirstTwo, Ytr, K );
+    Yhat = predict(learner,XtrFirstTwo);
+    errTrain(i) = ... % TODO: " " to count what fraction of predictions are wrong
+    %TODO: repeat prediction / error evaluation for test data
+end;
+figure; semilogx(... % TODO: " " to average and plot results on semi-log scale
+
+%}
