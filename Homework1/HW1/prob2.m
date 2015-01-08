788c7ca194a3023bb4e04feb35b3b0056e155b66
zackattacknyu <zackattacknyu@gmail.com>
2015-01-08 14:20:57 -0800

    started problem 2

--- Homework1/HW1/prob2.m
+++ Homework1/HW1/prob2.m
@@ -8,3 +8,10 @@ iris=load('data/iris.txt'); y=iris(:,end); X=iris(:,1:end-1);
 % (This is a good idea in case your data are ordered in some pathological way,
 % as the Iris data are)
 [Xtr Xte Ytr Yte] = splitData(X,y, .75); % split data into 75/25 train/test
+
+%train the classifier
+K = 3;
+knn = knnClassify( Xtr(:,1:2), Ytr, K ); % replace or set K to some integer
+YteHat = predict( knn, Xte(:,1:2) ); % make predictions on Xtest
+
+plotClassify2D( knn, Xtr(:,1:2), Ytr ); % make 2D classification plot with data (Xtr,Ytr)
