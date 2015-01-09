7749b8deae9368f6d7d0f130f0783645a446c340
zackattacknyu <zackattacknyu@gmail.com>
2015-01-09 11:24:50 -0800

    did more of prob3

--- Homework1/HW1/prob3.m
+++ Homework1/HW1/prob3.m
@@ -8,4 +8,20 @@ xyData=[
 0 0 1 0 0 1;
 1 0 0 0 0 1;
 1 0 1 1 0 1;
-1 1 1 1 1 -1];    
+1 1 1 1 1 -1];   
+
+X=xyData(:,1:5);
+y=xyData(:,6);
+
+%Part A
+
+%this gets p(y==1) and p(y==-1)
+indicesY1 = find(y==1);
+indicesYminus1 = find(y==-1);
+probY1 = length(indicesY1)/length(y);
+probYminus1 = 1-probY1;
+
+%in order to get class probabilities, we go over entries where
+%       y==1 and y==-1
+X1 = X(:,1);
+X1whereY1 = X(indicesY1);
