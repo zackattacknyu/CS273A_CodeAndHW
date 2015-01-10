3da9d773e95174f75c91b84fa07857b577c96499
zackattacknyu <zackattacknyu@gmail.com>
2015-01-09 17:20:41 -0800

    more of prob 3 done

--- Homework1/HW1/prob3.m
+++ Homework1/HW1/prob3.m
@@ -23,5 +23,29 @@ probYminus1 = 1-probY1;
 
 %in order to get class probabilities, we go over entries where
 %       y==1 and y==-1
+%
+% We will get the following matrices
+%   probXwhereY1:
+%       row i has probabilities for x_i
+%       column 1 is p(x_i=0|y=1)
+%       column 2 is p(x_i=1|y=1)
+%   probXwhereYminus1:
+%       row i has probabilities for x_i
+%       column 1 is p(x_i=0|y=-1)
+%       column 2 is p(x_i=1|y=-1)
 X1 = X(:,1);
-X1whereY1 = X(indicesY1);
+X1whereY1 = X1(indicesY1); %x1 values for entries where y=1
+X1whereYminus1 = X1(indicesYminus1); %x1 values for entries where y=-1
+probXwhereY1 = zeros(4,2);
+probXwhereYminus1 = zeros(4,2);
+for i = 1:4
+    Xi = X(:,i);
+    
+    XiwhereY1 = Xi(indicesY1);
+    probXwhereY1(i,1) = length(find(XiwhereY1==0))/length(XiwhereY1);
+    probXwhereY1(i,2) = length(find(XiwhereY1==1))/length(XiwhereY1);
+    
+    XiwhereYminus1 = Xi(indicesYminus1);
+    probXwhereYminus1(i,1) = length(find(XiwhereYminus1==0))/length(XiwhereYminus1);
+    probXwhereYminus1(i,2) = length(find(XiwhereYminus1==1))/length(XiwhereYminus1);
+end
