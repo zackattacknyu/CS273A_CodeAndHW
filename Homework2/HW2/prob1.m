73e280b7f6c08be3827c4cb883ecf4306b766f89
zackattacknyu <zackattacknyu@gmail.com>
2015-01-14 18:22:00 -0800

    started prob1

---
+++ Homework2/HW2/prob1.m
@@ -0,0 +1,10 @@
+%%
+%InitialPart
+iris=load('data/curve80.txt'); 
+y=iris(:,2); 
+X=iris(:,1);
+
+[X y] = shuffleData(X,y); % shuffle data randomly
+[Xtr Xte Ytr Yte] = splitData(X,y, .75); % split data into 75/25 train/test
+
+
