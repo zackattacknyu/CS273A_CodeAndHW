1edcd6fe5a38d4ebc5833bf06e264692a7700abb
zackattacknyu <zackattacknyu@gmail.com>
2015-01-16 19:09:08 -0800

    continued working on hw2

--- Homework2/HW2/prob1.m
+++ Homework2/HW2/prob1.m
@@ -23,3 +23,35 @@ YhatTr = predict(lr,Xtr); %gets predicted y for training data
 YhatTe = predict(lr,Xte); %gets predicted y for test data
 mseTr = sum(abs(YhatTr-Ytr).^2);
 mseTe = sum(abs(YhatTe-Yte).^2);
+
+%%
+%Part C
+
+%Xtr2 = [Xtr, Xtr.^2];
+
+degree=3;
+% create poly features up to given degree; no "1" feature
+XtrP = fpoly(Xtr, degree, false); 
+
+[XtrP, M,S] = rescale(XtrP); % it's often a good idea to scale the features
+lr = linearRegress( XtrP, Ytr ); % create and train model
+
+% defines an "implicit function" Phi(x)
+Phi = @(x) rescale( fpoly(x,degree,false), M,S); 
+
+% parameters "degree", "M", and "S" are memorized at the function definition
+% Now, Phi will do the required feature expansion and rescaling:
+YhatTrain = predict( lr, Phi(Xtr) ); % predict on training data
+YhatTest = predict(lr, Phi(Xte) ); 
+xs = (0:.05:10)'; % densely sample possible x-values
+ys = predict( lr, Phi(xs) ); % make predictions at xs
+plot(xs,ys)
+hold on
+plot(Xtr,Ytr,'g.');
+plot(Xte,Yte,'rx');
+
+
+
+
+
+
