71b9e8429f6356a0839486f83b57794a56cd1d98
zackattacknyu <zackattacknyu@gmail.com>
2015-01-24 01:49:45 -0800

    did more for part a

--- Homework3/HW3/prob1.m
+++ Homework3/HW3/prob1.m
@@ -2,8 +2,6 @@ iris=load('data/iris.txt'); % load the text file
 X = iris(:,1:2); Y=iris(:,end); % get first two features
 [X Y] = shuffleData(X,Y); % reorder randomly
 X = rescale(X); % works much better for rescaled data
-%XA = X(Y<2,:); YA=Y(Y<2); % get class 0 vs 1
-%XB = X(Y>0,:); YB=Y(Y>0); % get class 1 vs 2
 
 %part A
 X0 = X(Y==0,:); Y0=Y(Y==0);
@@ -11,16 +9,16 @@ X1 = X(Y==1,:); Y1=Y(Y==1);
 X2 = X(Y==2,:); Y2=Y(Y==2);
 
 %%
+%plot class 0 and class 1
 figure
 plot(X0(:,1),X0(:,2),'r.');
 hold on
 plot(X1(:,1),X1(:,2),'gx');
-%plot(X2(:,1),X2(:,2),'bo');
 hold off
 
-%%
+%plot class 1 and class 2
 figure
-plot(X1,'r.');
+plot(X1(:,1),X1(:,2),'gx');
 hold on
-plot(X2,'gx');
+plot(X2(:,1),X2(:,2),'bo');
 hold off
