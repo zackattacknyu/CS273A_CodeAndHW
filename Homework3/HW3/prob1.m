4956f424f10c872b9cfa1692ac7b009cd895b505
zackattacknyu <zackattacknyu@gmail.com>
2015-01-24 16:41:32 -0800

    began 1b write up

--- Homework3/HW3/prob1.m
+++ Homework3/HW3/prob1.m
@@ -3,32 +3,26 @@ X = iris(:,1:2); Y=iris(:,end); % get first two features
 [X Y] = shuffleData(X,Y); % reorder randomly
 X = rescale(X); % works much better for rescaled data
 
-%part A
-X0 = X(Y==0,:); Y0=Y(Y==0);
-X1 = X(Y==1,:); Y1=Y(Y==1);
-X2 = X(Y==2,:); Y2=Y(Y==2);
+XA = X(Y<2,:); YA=Y(Y<2); % get class 0 vs 1
+XB = X(Y>0,:); YB=Y(Y>0); % get class 1 vs 2
 
 %%
+%part A
+
 %plot class 0 and class 1
 figure
-plot(X0(:,1),X0(:,2),'r.');
-hold on
-plot(X1(:,1),X1(:,2),'gx');
-legend('Class 0','Class 1');
-hold off
+scatter(XA(:,1),XA(:,2),20,YA); %plot the data points
+colormap winter;
+colorbar
 
 %plot class 1 and class 2
 figure
-plot(X1(:,1),X1(:,2),'gx');
-hold on
-plot(X2(:,1),X2(:,2),'bo');
-legend('Class 1','Class 2','Location','SouthEast');
-hold off
+scatter(XB(:,1),XB(:,2),20,YB); %plot the data points
+colormap winter;
+colorbar
 
 %%
 %Part B
-XA = X(Y<2,:); YA=Y(Y<2); % get class 0 vs 1
-XB = X(Y>0,:); YB=Y(Y>0); % get class 1 vs 2
 
 %weights to demo for part b
 wts = [0.5 1 -0.25];
