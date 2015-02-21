4daa1b93b5634dcc2bdc7f8f9afb694a9df5a505
zackattacknyu <zackattacknyu@gmail.com>
2015-02-20 17:31:00 -0800

    fixed prob4

--- Homework4/HW4/prob4.m
+++ Homework4/HW4/prob4.m
@@ -7,7 +7,7 @@ Ytr = load('kaggle/kaggle.Y.train.txt');
 
 %start with "mean" predictor
 mu = mean(Ytrain); 
-curY = Ytrain - mu; 
+curY = 0; 
 
 %number of ensembles
 %N = 25;
@@ -17,22 +17,28 @@ mseTraining = zeros(1,N);
 mseValidation = zeros(1,N);
 
 %alpha values
-alpha = ones(1,N);
+%alpha = ones(1,N);
+%for i = 1:N
+%   alpha(i) = alpha(i)/i; 
+%end
+alpha = 0.25*ones(1,N);
 dt = cell(1,N);
 
 [Nvalid,Dval] = size(Xvalid);
 
-predictY = zeros(Nvalid,1); % Allocate space
+predictY = 0; %zeros(Nvalid,1); % Allocate space
 
 for k=1:N,
- dt{k} = treeRegress(Xtrain,curY,'maxDepth',3);
+ grad = 2*(curY - Ytrain);
+ dt{k} = treeRegress(Xtrain,grad,'maxDepth',3);
+ %dt{k} = treeRegress(Xtrain,curY,'maxDepth',20,'minParent',2^10);
+ %curY = curY - alpha(k) * predict(dt{k}, Xtrain);
  curY = curY - alpha(k) * predict(dt{k}, Xtrain);
- 
  %find training MSE at k
  mseTraining(k) = mean((curY-Ytrain).^2);
  
  %find validation MSE
- predictY = predictY + alpha(k)*predict(dt{k}, Xvalid);
+ predictY = predictY - alpha(k)*predict(dt{k}, Xvalid);
  mseValidation(k) = mean((Yvalid-predictY).^2);
  
 end;
