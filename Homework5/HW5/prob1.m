719d4dbf7682454009e7c7d18db7f157e7654f1e
zackattacknyu <zackattacknyu@gmail.com>
2015-02-27 00:46:37 -0800

    did prob1b

--- Homework5/HW5/prob1.m
+++ Homework5/HW5/prob1.m
@@ -4,4 +4,18 @@ X = iris(:,1:2);
 Y = iris(:,5);
 
 %%
+
+%Part A
 plot(X(:,1),X(:,2),'ro')
+
+%%
+
+%Part B
+k=5;
+[z,c,~] = kmeans(X,k);
+
+figure
+scatter(X(:,1),X(:,2),20,z); %plot the data points
+colorbar
+hold on
+plot(c(:,1),c(:,2),'rx');
