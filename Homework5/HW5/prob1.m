5f4bd3241d71244023e44f9e6daeba6b5a1b091b
zackattacknyu <zackattacknyu@gmail.com>
2015-02-27 01:35:20 -0800

    made better cluster centers for k-Means

--- Homework5/HW5/prob1.m
+++ Homework5/HW5/prob1.m
@@ -11,14 +11,17 @@ plot(X(:,1),X(:,2),'ro')
 %%
 
 %Part B
-k=5;
-%k=20;
+%k=5;
+k=20;
 
-%make 5 initial points with star shape
 minX1 = min(X(:,1)); 
 minX2 = min(X(:,2));
 maxX1 = max(X(:,1));
 maxX2 = max(X(:,2));
+
+%If k=5
+%make 5 initial points arranged in X-shape
+%{
 centerX1 = (minX1 + maxX1)/2; centerX2 = (minX2 + maxX2)/2;
 firstQuatX1 = minX1 + (maxX1-minX1)/4;
 thirdQuatX1 = maxX1 - (maxX1-minX1)/4;
@@ -29,13 +32,40 @@ Pt11 = [firstQuatX1 firstQuatX2];
 Pt13 = [firstQuatX1 thirdQuatX2];
 Pt31 = [thirdQuatX1 firstQuatX2];
 Pt33 = [thirdQuatX1 thirdQuatX2];
-initPts = [centerPt;Pt11;Pt13;Pt31;Pt33];
+initPts5 = [centerPt;Pt11;Pt13;Pt31;Pt33];
+%}
 
+%If k=20
+%make 20 points in 4x5 and 5x4 arrangement
+fifthX1 = (maxX1-minX1)/5;
+sixthX1 = (maxX1-minX1)/6;
+fifthX2 = (maxX2-minX2)/5;
+sixthX2 = (maxX2-minX2)/6;
+initPts20A = zeros(20,2);
+initPts20B = zeros(20,2);
+index = 1;
+for i = 1:5
+   for j = 1:4
+      curX1A = minX1 + fifthX1*j;
+      curX2A = minX2 + sixthX2*i;
+      initPts20A(index,:) = [curX1A curX2A];
+      
+      curX1B = minX1 + sixthX1*i;
+      curX2B = minX2 + fifthX2*j;
+      initPts20B(index,:) = [curX1B curX2B];
+      
+      index = index+1;
+   end
+end
+
+%%
 %run k-Means with the different initializations
 [z1,c1,score1] = kmeans(X,k,'random');
 [z2,c2,score2] = kmeans(X,k,'farthest');
 [z3,c3,score3] = kmeans(X,k,'k++');
-[z4,c4,score4] = kmeans(X,k,initPts);
+%[z4,c4,score4] = kmeans(X,k,initPts5);
+[z5,c5,score5] = kmeans(X,k,initPts20A);
+[z6,c6,score6] = kmeans(X,k,initPts20B);
 
 %%
 figure
