863dc16d85ac10bd9cda4ab47dba182242a5df24
zackattacknyu <zackattacknyu@gmail.com>
2015-03-07 20:03:06 -0800

    started prob3

--- Homework5/HW5/prob3.m
+++ Homework5/HW5/prob3.m
@@ -1,4 +1,11 @@
+%%
 X = load('data/faces.txt'); % load face dataset
 i=5;
 img = reshape(X(i,:),[24 24]); % convert vectorized datum to 24x24 image patch
 imagesc(img); axis square; colormap gray; % display an image patch; you may have to squint
+
+%%
+%Part A
+mu = mean(X,2);
+muMat = repmat(mu,1,576);
+X_0 = X-muMat;
