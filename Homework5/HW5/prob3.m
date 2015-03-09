86651e902a46b5cf7dfeef02932f53c8b4999a4f
zackattacknyu <zackattacknyu@gmail.com>
2015-03-08 18:48:28 -0700

    completed more of hw5

--- Homework5/HW5/prob3.m
+++ Homework5/HW5/prob3.m
@@ -23,13 +23,16 @@ for K=1:10
    mseSVD(K) = mean( mean( (X_0hat-X_0).^2 ) );
 end
 plot(mseSVD);
+xlabel('K value');
+ylabel('Mean Squared Error');
+title('MSE as function of K');
 
 %%
 
 %%
 %Part D
-j=3;
-alpha = 250;
+j=5;
+alpha = 10;
 img1 = reshape(mu(j) + alpha*V(j,:)', [24 24]);
 img2 = reshape(mu(j) - alpha*V(j,:)', [24 24]);
 
@@ -38,4 +41,26 @@ imagesc(img1); axis square; colormap gray;
 
 figure
 imagesc(img2); axis square; colormap gray;
+
+%%
+
+%Part E
+idx = 20:30; % pick some data at random or otherwise
+figure; hold on; axis ij; colormap(gray);
+range = max(W(idx,1:2)) - min(W(idx,1:2)); % find range of coordinates to be plotted
+scale = [200 200]./range; % want 24x24 to be visible but not large on new scale
+for i=idx, imagesc(W(i,1)*scale(1),W(i,2)*scale(2), reshape(X(i,:),24,24)); end;
+
+%%
+
+%Part F
+K=50; %need to test K=5,10,50
+faceNum = 15; %test 10,15
+X_0hat = W(:,1:K)*(V(:,1:K)');
+imgRecon = reshape(X_0hat(faceNum,:),[24 24]);
+figure
+imagesc(imgRecon); axis square; colormap gray;
+figure
+imagesc(reshape(X(faceNum,:),[24 24])); axis square; colormap gray;
+
 
